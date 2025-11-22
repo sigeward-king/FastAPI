b37a943965d2ab6fa245eb6fa00501d2121ece8f
sigeward-king <n2986463241@gmail.com>
2025-11-22 14:51:59 +0800

    feat: Compeleted response code in /pengpeng 2_main_response.py

--- pengpeng/2_main_response.py
+++ pengpeng/2_main_response.py
@@ -7,6 +7,8 @@ from fastapi.responses import PlainTextResponse
 
 from fastapi.responses import FileResponse
 
+from fastapi.responses import RedirectResponse
+
 app = FastAPI()
 
 """
@@ -18,12 +20,28 @@ app = FastAPI()
 4. responese 整个文件，使用 FileResponse 来返回。
 """
 
-
 @app.get("/")
 def index():
     return HTMLResponse("""
           <h3>网页标题</h3>
           <div>网页内容</div>
+          <a href='https://www.google.com'>Google</a>
     """)
 
+# 这样返回的就是一个 JSON 的格式：
+@app.get("/")
+def index():
+      return JSONResponse({"data": "Home Page", "title": "My Data"})
+
+
+# 这样会返回一个 HTML 的格式：
+@app.get("/")
+def index():
+      return FileResponse("www/home.html")
+
+
+# 返回一个图片的格式：
+@app.get("/img/logo")
+def logo():
+      return FileResponse("www/logo.png")
 
